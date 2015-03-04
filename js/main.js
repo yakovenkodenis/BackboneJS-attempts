2138c6fbd87488d7932525e4151067e372d8d16c
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-03-04 17:59:05 +0200

    Yet another commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ var Person = Backbone.Model.extend({
 		age: 23,
 		job: "Developer"
 	},
+
+	validate: function( attrs ) {
+		console.log(attrs);
+
+		if( attrs.age <= 0 ) {
+			return 'The age should be positive!';
+		}
+	},
+
 	walk: function() {
 		return this.get('name') + " is walking.";
 	}
